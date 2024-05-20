2c4f539fc5c41b36fabd56a3aa55d8a9e8079bb9
febry40 <febryevayonisa@gmail.com>
2024-05-20 19:49:38 +0700

    menambahkan int main dan akhiran codingan

--- inheritance.cpp
+++ inheritance.cpp
@@ -32,3 +32,10 @@ class pelajar : public orang {
     }
 };
 
+int main () {
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil =" << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}

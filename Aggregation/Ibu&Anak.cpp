0066419cb8d683fe4333b5488c15958234cf33ec
febry40 <febryevayonisa@gmail.com>
2024-05-20 15:25:59 +0700

    memanggil metode tambahanak

--- Aggregation/Ibu&Anak.cpp
+++ Aggregation/Ibu&Anak.cpp
@@ -8,4 +8,8 @@ int main() {
 	anak* varAnak2 = new anak("rini");
 	anak* varAnak3 = new anak("Dewi");
 
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
+	varibu2->tambahAnak(varAnak3);
+	varibu2->tambahAnak(varAnak1);
 }

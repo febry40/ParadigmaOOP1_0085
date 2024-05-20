4233e32bd6c0f5e9e5f59b23c8959fa3714b93a5
febry40 <febryevayonisa@gmail.com>
2024-05-20 14:32:14 +0700

    Add project files.

---
+++ ParadigmaOOP1_0085/ParadigmaOOP1_0085.cpp
@@ -0,0 +1,14 @@
+// ParadigmaOOP1_0085.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	const int id;
+	string nama;
+	float nilai;
+
+};
